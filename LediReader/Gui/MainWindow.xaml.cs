2193a8f32ef97af4ad70ed47db5cb1d7483635f1
Dirk Lellinger <dlellinger@users.sourceforge.net>
2019-10-03 20:15:57 +0200

    Move MainWindow into Gui namespace

--- LediReader/Gui/MainWindow.xaml.cs
+++ LediReader/Gui/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace LediReader
+namespace LediReader.Gui
 {
   /// <summary>
   /// Interaction logic for MainWindow.xaml
